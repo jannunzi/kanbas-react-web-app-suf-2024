cb682a2753d2fbcbcc277cdbae00f1bb14f99613
Jose Annunziato <jannunzi@gmail.com>
2024-05-08 21:18:28 -0400

    first commit

---
+++ src/Labs/index.tsx
@@ -0,0 +1,13 @@
+import Lab1 from "./Lab1";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+
+export default function Labs() {
+  return (
+    <div>
+      <Lab1 />
+      <Lab2 />
+      <Lab3 />
+    </div>
+  );
+}
