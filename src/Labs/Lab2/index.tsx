859e5d21d22b55440ce110f908153307d9a025cd
Jose Annunziato <jannunzi@gmail.com>
2024-05-08 21:28:44 -0400

    ex 1, 2, 3, 4

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -2,6 +2,10 @@ export default function Lab2() {
   return (
     <div>
       <h2>Lab 2</h2>
+      <h3>Exercise 1</h3>
+      <h3>Exercise 2</h3>
+      <h3>Exercise 3</h3>
+      <h3>Exercise 4</h3>
     </div>
   );
 }
